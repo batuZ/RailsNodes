a55d2a33d779320836bd499c6d3226db446f7584
batu <304701204@qq.com>
2017-09-06 12:38:21 +0800

    11

--- rails_杂记.rb
+++ rails_杂记.rb
@@ -1,4 +1,6 @@
 require 'Win32API'
+require 'rubygems'
+#require 'mysql2'
+#ss = Win32API.new("E:\\test\\test04\\testDll\\x64\\Debug\\testDll.dll", "sun",['I','I'], 'I')
 
-ss = Win32API.new("E:\\test\\test04\\testDll\\x64\\Debug\\testDll.dll", "sun",['I','I'], 'I')
-
+gem install mysql2 –with-mysql-config='C:\Program Files\MySQL\MySQL Server 5.7\binmysql_config'
