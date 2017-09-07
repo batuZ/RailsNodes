a0ccc7381f2810a5ff6b63a0ac9a569e3f5a6c60
batu <304701204@qq.com>
2017-09-07 17:28:23 +0800

    add sqlite3 what can i do

--- rails_杂记.rb
+++ rails_杂记.rb
@@ -4,3 +4,12 @@ require 'rubygems'
 #ss = Win32API.new("E:\\test\\test04\\testDll\\x64\\Debug\\testDll.dll", "sun",['I','I'], 'I')
 
 gem install mysql2 –with-mysql-config='C:\Program Files\MySQL\MySQL Server 5.7\binmysql_config'
+
+操作sqlite3
+ATTACH DATABASE 'E:\RailsProject\program\development.sqlite3' As 'Al';
+SELECT name,id FROM Al.projects WHERE id=1;
+.database
+.tables
+
+SELECT * FROM Al.groups where project_id=1;
+UPDATE COMPANY SET ADDRESS = 'Texas' WHERE ID = 6;
